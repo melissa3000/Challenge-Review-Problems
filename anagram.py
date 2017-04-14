c6477576b81b5c714f5b3e24b0fc12fccb3e2c71
Melissa Boyer <melissaboyer3000@gmail.com>
2017-04-14 12:40:46 -0700

    Added another solution to Anagram

--- anagram.py
+++ anagram.py
@@ -2,18 +2,32 @@ def anagram(s1,s2):
 
     # Solution 1
 
-    # s1 = s1.lower()
-    # s2 = s2.lower()
+    count = {}
 
-    # s1 = s1.replace(' ', '')
-    # s2 = s2.replace(' ', '')
-
-    # lst1 = list(s1)
-    # lst2 = list(s2)
+    s1 = s1.replace(' ', '').lower()
+    s2 = s2.replace(' ', '').lower()
 
 
-    # if len(s1) != len(s2):
-    #     return False
+    if len(s1) != len(s2):
+        return False
+
+        for letter in s1:
+            if letter in count:
+                count[letter] += 1
+            else:
+                letter[count] = 1
+
+        for letter in s2:
+            if letter in count:
+                count[letter] -= 1
+            else:
+                letter[count] = 1
+
+    for num in count.values():
+        if num > 0:
+            return False
+
+    return True
 
     # for i in range(len(lst1)):
     #     if lst1[i] not in lst2:
@@ -24,10 +38,10 @@ def anagram(s1,s2):
 
     # Solution 2
 
-    s1 = s1.replace(' ', '').lower()
-    s2 = s2.replace(' ', '').lower()
+    # s1 = s1.replace(' ', '').lower()
+    # s2 = s2.replace(' ', '').lower()
 
-    return sorted(s1) == sorted(s2)
+    # return sorted(s1) == sorted(s2)
 
 
 
