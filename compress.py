7e6aec6b5dcecfac9014043db18363a4653c2837
Melissa Boyer <melissaboyer3000@gmail.com>
2017-04-17 17:13:50 -0700

    Added another solution

--- compress.py
+++ compress.py
@@ -17,5 +17,30 @@ def compress(s):
     print result
 
 
+    #Solution 2
+    # r = ''
+
+    # if len(s) == 0:
+    #     return ''
+
+    # if len(s) == 1:
+    #     return s + '1'
+
+    # last = s[0]
+    # count = 1
+    # i = 1
+
+    # while i < len(s):
+    #     if s[i] == s[i - 1]:
+    #         count += 1
+    #     else:
+    #         r = r + s[i-1] + str(count)
+    #         count = 1
+
+    #     i += 1
+
+    # r = r + s[i-1] + str(count)
+    # return r
+
 compress('AAAAABBBBCCCC') #'A5B4C4'
 compress('AABBCC') # 'A2B2C2'
