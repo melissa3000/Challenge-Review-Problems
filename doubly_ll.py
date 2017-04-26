327aebb4352fb1352498547adac3d03dbdf72e50
Melissa Boyer <melissaboyer3000@gmail.com>
2017-04-26 08:19:10 -0700

    Implement Singly and Doubly Linked Lists

---
+++ doubly_ll.py
@@ -0,0 +1,11 @@
+# Implement a Doubly Linked List
+
+class Node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.nextnode = None
+        self.previous = None
+        self.head = None
+
+
---
+++ singly_ll.py
@@ -0,0 +1,11 @@
+# Implement a Singly Linked List
+
+class LLNode(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.nextnode = None
+        self.head = None
+
+
+
